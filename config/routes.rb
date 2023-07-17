faea4bcb7c9ee3cf4179c5439f25b4598b8e188b
Jay Kava <jay.logicalstreet@gmail.com>
2023-07-17 17:39:58 +0530

    all rubocop auto fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :students
-  
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
